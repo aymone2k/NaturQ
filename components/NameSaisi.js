0cdde1fb5202922d6538887b72f59739c13e31d3
aymone2k <73581149+aymone2k@users.noreply.github.com>
2021-02-17 17:26:28 +0100

    a revoir name saisi

---
+++ components/NameSaisi.js
@@ -0,0 +1,17 @@
+import { RecyclerViewBackedScrollViewBase } from "react-native";
+
+import React, {Component, useState} from 'react';
+import { StyleSheet, Text, Image, TextInput, View, Button, TouchableWithoutFeedback } from 'react-native';
+import Name from '../components/Name.js'
+
+
+export default function NameSaisi( {...nom}) {
+
+    
+        
+        return<View>
+             <Text>{nom}</Text>
+      
+          
+           </View>
+}
